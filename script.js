a5cbd9447eeb9a62dfcaee601a3ec86d72c441a4
alocasia-polly <wendy-shaw@outlook.com>
2024-06-09 22:59:09 -0700

    Updated dog info variables to be declared as constants

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-let infoMurphy = ['Murphy', 'Mix', 120.00];
-let infoPoppy = ['Poppy', 'Havanese', 123.45];
-let infoJack = ['Jack', 'Beagle', 110.20];
-let infoDuffy = ['Duffy', 'Pit Bull', 125.35];
-let infoLucas = ['Lucas', 'Dutch Shepherd', 105.00];
-let infoJake = ['Jake', 'Yellow Lab', 100.00];
-let infoAngus = ['Angus', 'Boxer', 107.00];
-let infoViolet = ['Violet', 'Chocolate Lab', 140.50];
-let infoPiper = ['Piper', 'American Shepherd', 130.15];
-let infoMaximus = ['Maximus', 'Russell Terrier', 122.80];
-let infoLuna = ['Luna', 'Yellow Lab', 123.40];
-let infoStella = ['Stella', 'Chihuahua', 80.00];
+const infoMurphy = ['Murphy', 'Mix', 120.00];
+const infoPoppy = ['Poppy', 'Havanese', 123.45];
+const infoJack = ['Jack', 'Beagle', 110.20];
+const infoDuffy = ['Duffy', 'Pit Bull', 125.35];
+const infoLucas = ['Lucas', 'Dutch Shepherd', 105.00];
+const infoJake = ['Jake', 'Yellow Lab', 100.00];
+const infoAngus = ['Angus', 'Boxer', 107.00];
+const infoViolet = ['Violet', 'Chocolate Lab', 140.50];
+const infoPiper = ['Piper', 'American Shepherd', 130.15];
+const infoMaximus = ['Maximus', 'Russell Terrier', 122.80];
+const infoLuna = ['Luna', 'Yellow Lab', 123.40];
+const infoStella = ['Stella', 'Chihuahua', 80.00];
 
 let currentTotal = null;
 
